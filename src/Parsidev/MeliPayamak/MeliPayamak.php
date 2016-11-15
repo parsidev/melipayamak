782d3e9919c3a7579a5599dd821240062f05e652
Mohammad Reza <parsidev@users.noreply.github.com>
2016-11-15 22:48:50 +0330

    Update MeliPayamak.php

--- src/Parsidev/MeliPayamak/MeliPayamak.php
+++ src/Parsidev/MeliPayamak/MeliPayamak.php
@@ -2,103 +2,66 @@
 
 namespace Parsidev\MeliPayamak;
 
-class MeliPayamak {
+class MeliPayamak
+{
 
     protected $confg;
     protected $client;
 
-    public function __construct($config, $client) {
+    public function __construct($config, $client)
+    {
         $this->confg = $config;
         $this->client = $client;
     }
 
-    public static function CorrectNumber(&$uNumber) {
-        $uNumber = Trim($uNumber);
-        $ret = &$uNumber;
-
-        if (substr($uNumber, 0, 3) == '%2B') {
-            $ret = substr($uNumber, 3);
-            $uNumber = $ret;
-        }
-
-        if (substr($uNumber, 0, 3) == '%2b') {
-            $ret = substr($uNumber, 3);
-            $uNumber = $ret;
-        }
-
-        if (substr($uNumber, 0, 4) == '0098') {
-            $ret = substr($uNumber, 4);
-            $uNumber = $ret;
-        }
-
-        if (substr($uNumber, 0, 3) == '098') {
-            $ret = substr($uNumber, 3);
-            $uNumber = $ret;
-        }
-
-
-        if (substr($uNumber, 0, 3) == '+98') {
-            $ret = substr($uNumber, 3);
-            $uNumber = $ret;
-        }
-
-        if (substr($uNumber, 0, 2) == '98') {
-            $ret = substr($uNumber, 2);
-            $uNumber = $ret;
-        }
-
-        if (substr($uNumber, 0, 1) == '0') {
-            $ret = substr($uNumber, 1);
-            $uNumber = $ret;
-        }
-
-        return '+98' . $ret;
+    public function getStatuses($uniqueId)
+    {
+        if (!is_array($uniqueId))
+            $uniqueId = [$uniqueId];
+        $parameters['username'] = $this->confg['Username'];
+        $parameters['password'] = $this->confg['Password'];
+        $parameters['recIds'] = $uniqueId;
+        $response = $this->client->GetDeliveries($parameters)->GetDeliveriesResult;
+        return $response;
     }
 
-    public function getStatus($uniqueId) {
-        $userName = $this->confg['Username'];
-        $password = $this->confg['Password'];
-        if (is_array($uniqueId)) {
-            $i = sizeOf($uniqueId);
+    public function getStatus($uniqueId)
+    {
+        $parameters['username'] = $this->confg['Username'];
+        $parameters['password'] = $this->confg['Password'];
+        $parameters['recId'] = $uniqueId;
+        $response = $this->client->GetDelivery2($parameters)->GetDeliveryResult;
+        return $response;
+    }
 
-            while ($i--) {
-                $uniqueId[$i] = $uniqueId[$i];
-            }
-        } else {
-            $uniqueId = array($uniqueId);
+    public function sendSMS($to, $message, $type = 'normal')
+    {
+        if (!is_array($to))
+            $to = [$to];
+        $parameters['username'] = $this->confg['Username'];
+        $parameters['password'] = $this->confg['Password'];
+        $parameters['to'] = $to;
+        $parameters['from'] = $this->confg['fromNumber'];
+        $parameters['text'] = $message;
+        if ($type == 'normal')
+            $parameters['isflash'] = false;
+        elseif ($type == 'flash')
+            $parameters['isflash'] = true;
+
+        $response = $this->client->SendSimpleSMS($parameters)->SendSimpleSMSResult;
+
+        foreach ($response as $v) {
+            $response = $v;
         }
-
-        $response = $this->client->GetStatus(
-                $userName, $password, $uniqueId
-        );
 
         return $response;
     }
 
-    public function sendSMS($to, $message, $type = 'normal') {
-        $userName = $this->confg['Username'];
-        $password = $this->confg['Password'];
-        $fromNum = $this->confg['fromNumber'];
-        if (is_array($to)) {
-            $i = sizeOf($to);
-
-            while ($i--) {
-                $to[$i] = $this->CorrectNumber($to[$i]);
-            }
-        } else {
-            $to = array($this->CorrectNumber($to));
-        }
-        $response = $this->client->SendSMS(
-                $fromNum, $to, $message, $type, $userName, $password
-        );
-
-        return $response;
-    }
-
-    public function getCredit() {
-        $response = $this->client->GetCredit(
-                $this->confg['Username'], $this->confg['Password']
-        );
+    public function getCredit()
+    {
+        $parameters['username'] = $this->confg['Username'];
+        $parameters['password'] = $this->confg['Password'];
+        $response = $this->client->GetCredit($parameters)->GetCreditResult;
 
         return $response;
     }
