a6d9e1357ad3d8d3e4df7b0a6acb4991ee0f4ee2
Mohammad Reza <parsidev@users.noreply.github.com>
2017-07-21 13:02:28 +0430

    Update melipayamak.php

--- src/config/melipayamak.php
+++ src/config/melipayamak.php
@@ -1,7 +1,13 @@
 <?php
 
 return [
-    'webserviceUrl' => 'http://api.payamak-panel.com/post/Send.asmx?wsdl',
+    'SendUrl' => 'http://api.payamak-panel.com/post/Send.asmx?wsdl',
+    'ContactUrl' => 'http://api.payamak-panel.com/post/contacts.asmx?wsdl',
+    'ReceiveUrl' => 'http://api.payamak-panel.com/post/receive.asmx?wsdl',
+    'TicketUrl' => 'http://api.payamak-panel.com/post/Tickets.asmx?wsdl',
+    'UserUrl' => 'http://api.payamak-panel.com/post/users.asmx?wsdl',
+    'ScheduleUrl' => 'http://api.payamak-panel.com/post/Schedule.asmx?wsdl',
+    'RegionalUrl' => 'http://api.payamak-panel.com/post/Actions.asmx?wsdl',
     'Username' => '*****',
     'Password' => '*********',
     'fromNumber' => '*********',
