0410a6f09e090f4cdd8972db985a9c961802df3a
Mohammad Reza <parsidev@users.noreply.github.com>
2017-07-21 13:32:12 +0430

    Update MeliPayamakServiceProvider.php

--- src/Parsidev/MeliPayamak/MeliPayamakServiceProvider.php
+++ src/Parsidev/MeliPayamak/MeliPayamakServiceProvider.php
@@ -1,29 +1,20 @@
 <?php
-
 namespace Parsidev\MeliPayamak;
-
 use Illuminate\Support\ServiceProvider;
-use SoapClient;
-
 class MeliPayamakServiceProvider extends ServiceProvider {
-
     protected $defer = true;
-
     public function boot() {
         $this->publishes([
             __DIR__ . '/../../config/melipayamak.php' => config_path('melipayamak.php'),
         ]);
     }
-
     public function register() {
         $this->app->singleton('melipayamak', function($app) {
             $config = config('melipayamak');
-            return new MeliPayamak($config, new SoapClient($config['webserviceUrl'], ['encoding' => 'UTF-8']));
+            return new MeliPayamak($config);
         });
     }
-
     public function provides() {
         return ['melipayamak'];
     }
-
 }
